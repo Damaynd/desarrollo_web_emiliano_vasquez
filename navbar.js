db44dc1aaa98a99a931032bb188f08a72cc7e9cc
Damaynd <fromdamind@gmail.com>
2026-03-29 13:53:57 -0300

    Fix: issues related with navegation bar display and reorganization of code in registro.html

--- navbar.js
+++ navbar.js
@@ -1,5 +1,11 @@
 fetch("navbar.html")
-  .then(response => response.text())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("No se pudo cargar navbar.html");
+    }
+    return response.text();
+  })
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
-  });
+  })
+  .catch(error => console.error("Error cargando navbar:", error));
