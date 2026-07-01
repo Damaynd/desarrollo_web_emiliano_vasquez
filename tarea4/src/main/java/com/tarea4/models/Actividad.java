031424cf3f2f790f5b214de99d7a734f89f5cffe
Damaynd <fromdamind@gmail.com>
2026-06-30 20:36:11 -0400

    Feat: Actividad model in java

---
+++ tarea4/src/main/java/com/tarea4/models/Actividad.java
@@ -0,0 +1,68 @@
+package com.tarea4.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "actividad")
+public class Actividad {
+    @Id
+    private Integer id;
+
+    private String dia;
+
+    @Column(name = "hora_inicio")
+    private String horaInicio;
+
+    private String duracion;
+
+    private String tipo;
+
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "miembro_id")
+    
+    private Miembro miembro;
+
+    public Actividad() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Miembro getMiembro() {
+        return miembro;
+    }
+}
